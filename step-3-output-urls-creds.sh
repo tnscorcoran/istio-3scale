3798f12a281f3410d0ec7f888ec3578d3437566a
Tom Corcoran <tnscorcoran@gmail.com>
2018-11-23 14:08:22 +1100

    Committing files

--- step-3-output-urls-creds.sh
+++ step-3-output-urls-creds.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 
-echo "Openshift URL, ID and Password:"
+echo "Openshift URL, ID and Password:\n"
 echo "https://$OCP_DOMAIN:8443"
 echo $OCP_USERNAME
-echo -en $OCP_PASSWD
+echo $OCP_PASSWD
 
-echo "3scale Admin URL:"
-echo -en "\n\nhttps://$TENANT_NAME-admin.$API_WILDCARD_DOMAIN\n\n"   
+echo "\n3scale Admin URL:\n"
+echo "https://$TENANT_NAME-admin.$API_WILDCARD_DOMAIN\n"   
 
-echo "3scale Admin URL:"
+echo "3scale Admin URL:\n"
 echo "http://$NAKED_CATALOG_ROUTE"
 
    
