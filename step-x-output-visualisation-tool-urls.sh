9a919416c9d5ea0a5e87e7f7c2f4d6ad8ac6b2e6
Tom Corcoran <tnscorcoran@gmail.com>
2018-11-25 09:56:34 +1100

    Committing files

--- step-x-output-visualisation-tool-urls.sh
+++ step-x-output-visualisation-tool-urls.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Jaeger URL:"
-echo      $(oc get route/tracing -n istio-system -o template --template {{.spec.host}})
+echo -en "         http://"$(oc get route/tracing -o template --template {{.spec.host}} -n istio-system)"\n\n"
 echo
 echo "Grafana URL:"
-echo      $(oc get route/grafana -n istio-system -o template --template {{.spec.host}})
+echo -en "         http://"$(oc get route/grafana -o template --template {{.spec.host}} -n istio-system)"\n\n"
 echo
